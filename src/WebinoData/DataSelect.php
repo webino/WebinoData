2dd4bf3c21047bb29ce4d562746d17dabfe71210
Peter Bacinsky <peter@bacinsky.sk>
2013-07-02 20:01:47 +0200

    Updated DataSelect, added getSearch()

--- src/WebinoData/DataSelect.php
+++ src/WebinoData/DataSelect.php
@@ -11,6 +11,7 @@ class DataSelect
     protected $service;
     protected $sqlSelect;
     protected $subselects = array();
+    protected $search = array();
 
     public function __construct(DataService $service, Select $select)
     {
@@ -26,6 +27,11 @@ class DataSelect
     public function getColumns()
     {
         return $this->sqlSelect->getRawState('columns');
+    }
+
+    public function getSearch()
+    {
+        return $this->search;
     }
 
     public function columns(array $columns, $prefixColumnsWithTable = true)
@@ -138,6 +144,9 @@ class DataSelect
             return $this;
         }
 
+        in_array($term, $this->search) or
+            $this->search[] = $term;
+
         $this->sqlSelect->where('(' . join(' AND ', $where) . ')', $combination);
         return $this;
     }
@@ -168,149 +177,28 @@ class DataSelect
         return $this->sqlSelect->reset($part);
     }
 
+    /**
+     * Variable overloading
+     *
+     * @param  string $name
+     * @throws \InvalidArgumentException
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        switch (strtolower($name)) {
+            case 'where':
+                return $this->sqlSelect->where;
+            case 'having':
+                return $this->sqlSelect->having;
+            default:
+                throw new \InvalidArgumentException('Not a valid magic property for this object');
+        }
+    }
+
+
     public function __toString()
     {
         return $this->getSqlString($this->service->getPlatform());
-    }
-
-    /**
-     * TODO
-     *
-     * @param PlatformInterface $platform
-     * @param DriverInterface $driver
-     * @param ParameterContainer $parameterContainer
-     * @return null|array
-     */
-    protected function processSelect(PlatformInterface $platform, DriverInterface $driver = null, ParameterContainer $parameterContainer = null)
-    {
-        $expr = 1;
-
-        if (!$this->table) {
-            return null;
-        }
-
-        $table = $this->table;
-        $schema = $alias = null;
-
-        if (is_array($table)) {
-            $alias = key($this->table);
-            $table = current($this->table);
-        }
-
-        // create quoted table name to use in columns processing
-        if ($table instanceof TableIdentifier) {
-            list($table, $schema) = $table->getTableAndSchema();
-        }
-
-        if ($table instanceof Select) {
-            $table = '(' . $this->processSubselect($table, $platform, $driver, $parameterContainer) . ')';
-        } else {
-            $table = $platform->quoteIdentifier($table);
-        }
-
-        if ($schema) {
-            $table = $platform->quoteIdentifier($schema) . $platform->getIdentifierSeparator() . $table;
-        }
-
-        if ($alias) {
-            $fromTable = $platform->quoteIdentifier($alias);
-            $table = $this->renderTable($table, $fromTable);
-        } else {
-            $fromTable = $table;
-        }
-
-        if ($this->prefixColumnsWithTable) {
-            $fromTable .= $platform->getIdentifierSeparator();
-        } else {
-            $fromTable = '';
-        }
-
-        // process table columns
-        $columns = array();
-        foreach ($this->columns as $columnIndexOrAs => $column) {
-
-            $columnName = '';
-            if ($column === self::SQL_STAR) {
-                // todo
-                $columns[] = array($platform->getIdentifierSeparator() . self::SQL_STAR);
-                continue;
-            }
-
-            if ($column instanceof Expression) {
-                $columnParts = $this->processExpression(
-                    $column,
-                    $platform,
-                    $driver,
-                    $this->processInfo['paramPrefix'] . ((is_string($columnIndexOrAs)) ? $columnIndexOrAs : 'column')
-                );
-                if ($parameterContainer) {
-                    $parameterContainer->merge($columnParts->getParameterContainer());
-                }
-                $columnName .= $columnParts->getSql();
-            } else {
-                $columnName .= $fromTable . $platform->quoteIdentifier($column);
-            }
-
-            // process As portion
-            if (is_string($columnIndexOrAs)) {
-                $columnAs = $platform->quoteIdentifier($columnIndexOrAs);
-            } elseif (stripos($columnName, ' as ') === false) {
-                $columnAs = (is_string($column)) ? $platform->quoteIdentifier($column) : 'Expression' . $expr++;
-            }
-            $columns[] = (isset($columnAs)) ? array($columnName, $columnAs) : array($columnName);
-        }
-
-        $separator = $platform->getIdentifierSeparator();
-
-        // process join columns
-        foreach ($this->joins as $join) {
-            foreach ($join['columns'] as $jKey => $jColumn) {
-                $jColumns = array();
-                if ($jColumn instanceof ExpressionInterface) {
-                    $jColumnParts = $this->processExpression(
-                        $jColumn,
-                        $platform,
-                        $driver,
-                        $this->processInfo['paramPrefix'] . ((is_string($jKey)) ? $jKey : 'column')
-                    );
-                    if ($parameterContainer) {
-                        $parameterContainer->merge($jColumnParts->getParameterContainer());
-                    }
-                    $jColumns[] = $jColumnParts->getSql();
-                } else {
-                    $name = (is_array($join['name'])) ? key($join['name']) : $name = $join['name'];
-                    if ($name instanceof TableIdentifier) {
-                        $name = $platform->quoteIdentifier($name->getSchema()) . $separator . $platform->quoteIdentifier($name->getTable());
-                    } else {
-                        $name = $platform->quoteIdentifier($name);
-                    }
-                    $jColumns[] = $name . $separator . $platform->quoteIdentifierInFragment($jColumn);
-                }
-                if (is_string($jKey)) {
-                    $jColumns[] = $platform->quoteIdentifier($jKey);
-                } elseif ($jColumn !== self::SQL_STAR) {
-                    $jColumns[] = $platform->quoteIdentifier($jColumn);
-                }
-                $columns[] = $jColumns;
-            }
-        }
-
-        if ($this->quantifier) {
-            if ($this->quantifier instanceof Expression) {
-                $quantifierParts = $this->processExpression($this->quantifier, $platform, $driver, 'quantifier');
-                if ($parameterContainer) {
-                    $parameterContainer->merge($quantifierParts->getParameterContainer());
-                }
-                $quantifier = $quantifierParts->getSql();
-            } else {
-                $quantifier = $this->quantifier;
-            }
-        }
-
-        if (isset($quantifier)) {
-            return array($quantifier, $columns, $table);
-        } else {
-            return array($columns, $table);
-        }
     }
 }
