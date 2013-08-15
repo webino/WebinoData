fd509384303c6943e42f59d4116926e272031e11
Peter Bacinsky <peter@bacinsky.sk>
2013-08-15 13:07:45 +0200

    Fixed DataTimeStamp, added is optional

--- src/WebinoData/DataPlugin/DateTimeStamp.php
+++ src/WebinoData/DataPlugin/DateTimeStamp.php
@@ -26,13 +26,32 @@ class DateTimeStamp
 
         $dateTimeFormatter = new DateTimeFormatter();
         $dateTime = $dateTimeFormatter->filter(time());
-        
+
         $data['updated'] = $dateTime;
 
         if ($event->isUpdate()) {
             return;
         }
 
-        $data['added'] = $dateTimeFormatter->filter(time());
+        $service = $event->getService();
+        $config  = $service->getConfig();
+        unset($config['input_filter']['type']);
+
+        // todo PHP 5.5 array_column
+        $inputs = array_flip(
+            array_map(
+                function($value) {
+                    return $value['name'];
+                },
+                $config['input_filter']
+            )
+        );
+
+        if (empty($inputs['added'])) {
+            // added is optional
+            return;
+        }
+
+        $data['added'] = $dateTime;
     }
 }
