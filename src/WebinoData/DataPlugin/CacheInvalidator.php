74e71a727ee83c281cfd683c0cde4964233bff28
Peter Bacinsky <peter@bacinsky.sk>
2013-12-03 17:27:28 +0100

    Fixed DataPlugin\CacheInvalidator, set high priority to the event listeners

--- src/WebinoData/DataPlugin/CacheInvalidator.php
+++ src/WebinoData/DataPlugin/CacheInvalidator.php
@@ -28,11 +28,11 @@ class CacheInvalidator extends AbstractConfigurable
      */
     public function attach(EventManager $eventManager)
     {
-        $eventManager->attach('data.exchange.post', array($this, 'clearCache'));
-        $eventManager->attach('data.toggle', array($this, 'clearCache'));
-        $eventManager->attach('data.increment', array($this, 'clearCache'));
-        $eventManager->attach('data.decrement', array($this, 'clearCache'));
-        $eventManager->attach('data.delete', array($this, 'clearCache'));
+        $eventManager->attach('data.exchange.post', array($this, 'clearCache'), 100);
+        $eventManager->attach('data.toggle', array($this, 'clearCache'), 100);
+        $eventManager->attach('data.increment', array($this, 'clearCache'), 100);
+        $eventManager->attach('data.decrement', array($this, 'clearCache'), 100);
+        $eventManager->attach('data.delete', array($this, 'clearCache'), 100);
     }
 
     public function clearCache(DataEvent $event)
