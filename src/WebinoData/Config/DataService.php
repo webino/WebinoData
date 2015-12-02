a41c8a526d74f39b6292c3bac4164f11ad381a4c
Peter Bacinsky <peter@bacinsky.sk>
2015-12-02 20:09:06 +0100

    Enhanced DataService & InputFilter configurators

--- src/WebinoData/Config/DataService.php
+++ src/WebinoData/Config/DataService.php
@@ -3,12 +3,14 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoData for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
 
 namespace WebinoData\Config;
+
+use WebinoData\InputFilter\InputFilter as DataInputFilter;
 
 /**
  * Data service configurator
@@ -20,7 +22,13 @@ class DataService extends AbstractDataService
      */
     public function __construct(array $options)
     {
-        $this->options = $options;
+        foreach ($options as $key => $option) {
+            if ($option instanceof InputFilter\InputFilter) {
+                $this->options['parameters']['config']['input_filter'] = $option->toArray();
+            } else {
+                $this->options[$key] = $option;
+            }
+        }
     }
 
     /**
@@ -31,9 +39,7 @@ class DataService extends AbstractDataService
         return [
             'parameters' => [
                 'config' => [
-                    'input_filter' => [
-                        'type' => 'WebinoData\InputFilter\InputFilter',
-                    ],
+                    'input_filter' => ['type' => DataInputFilter::class],
                 ],
             ],
         ];
--- src/WebinoData/Config/InputFilter/InputFilter.php
+++ src/WebinoData/Config/InputFilter/InputFilter.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoData for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -21,8 +21,20 @@ class InputFilter
     protected $spec = [];
 
     /**
-     * @param InputInterface $input
-     * @return self
+     * @param array $input
+     */
+    public function __construct(array $input = [])
+    {
+        foreach ($input as $item) {
+            if ($item instanceof Input\InputInterface) {
+                $this->push($item);
+            }
+        }
+    }
+
+    /**
+     * @param Input\InputInterface $input
+     * @return $this
      */
     public function push(Input\InputInterface $input)
     {
