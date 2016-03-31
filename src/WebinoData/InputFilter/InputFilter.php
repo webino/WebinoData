eecbc259744a93bc2f9994d5cacd6695ebb232da
Peter Bacinsky <peter@bacinsky.sk>
2016-03-31 03:29:26 +0200

    Updated InputFilter, added excludeInputs() & excludeData()

--- src/WebinoData/InputFilter/InputFilter.php
+++ src/WebinoData/InputFilter/InputFilter.php
@@ -5,6 +5,7 @@ namespace WebinoData\InputFilter;
 use Zend\InputFilter\BaseInputFilter;
 use Zend\InputFilter\Input;
 use Zend\InputFilter\InputFilter as ZendInputFilter;
+use Zend\Validator\Db\AbstractDb;
 
 /**
  * Class InputFilter
@@ -51,28 +52,56 @@ class InputFilter extends ZendInputFilter
     }
 
     /**
+     * @param array $data
+     * @return $this
+     */
+    public function excludeInputs(array $data)
+    {
+        foreach ($this->getInputs() as $input) {
+            $key = $input->getName();
+            array_key_exists($key, $data) or $this->remove($key);
+        }
+        return $this;
+    }
+
+    /**
+     * Excludes undefined inputs from data
+     *
+     * @param array $data
+     * @return array
+     */
+    public function excludeData(array $data)
+    {
+        $newData = [];
+        foreach ($this->getInputs() as $input) {
+            $key = $input->getName();
+            array_key_exists($key, $data) and $newData[$key] = $data[$key];
+        }
+        return $newData;
+    }
+
+    /**
      * @param Input $input
      * @param array $data
      * @return $this
      */
     protected function prepareInputValidators(Input $input, array $data)
     {
-        !empty($data['id']) or $data['id'] = 0;
+        empty($data['id']) and $data['id'] = 0;
         $data['inputName'] = $input->getName();
 
         foreach ($input->getValidatorChain()->getValidators() as $validator) {
             // Db validator
-            if ($validator['instance'] instanceof \Zend\Validator\Db\AbstractDb) {
+            if ($validator['instance'] instanceof AbstractDb) {
                 $exclude = $validator['instance']->getExclude();
 
-                is_array($exclude)
-                    and $exclude['value'] = !empty($data[$exclude['value']])
-                                         ? $data[$exclude['value']]
-                                         : $exclude['value'];
+                if (is_array($exclude)) {
+                    $exclude['value'] = !empty($data[$exclude['value']])
+                        ? $data[$exclude['value']]
+                        : $exclude['value'];
+                }
 
-                is_string($exclude)
-                    and $exclude = $this->translateExclude($exclude, $data);
-
+                is_string($exclude) and $exclude = $this->translateExclude($exclude, $data);
                 $validator['instance']->setExclude($exclude);
             }
         }
@@ -87,7 +116,7 @@ class InputFilter extends ZendInputFilter
      */
     protected function translateExclude($exclude, array $data)
     {
-        $translation = array();
+        $translation = [];
 
         foreach ($data as $key => $value) {
             if (!is_array($value)) {
