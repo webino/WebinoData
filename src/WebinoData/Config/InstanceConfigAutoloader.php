e36f010ba715b302003af26922af38ac4f329c68
Peter Bacinsky <peter@bacinsky.sk>
2016-04-10 16:57:58 +0200

    Updated Config\InstanceConfigAutoloader, added default table setting

--- src/WebinoData/Config/InstanceConfigAutoloader.php
+++ src/WebinoData/Config/InstanceConfigAutoloader.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoData for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2016 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -13,31 +13,52 @@ namespace WebinoData\Config;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use RegexIterator;
+use WebinoData\DataService;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\TableGateway\TableGateway;
 use Zend\Filter\Word\DashToCamelCase;
 use Zend\Stdlib\ArrayUtils;
 
 /**
- *
+ * Class InstanceConfigAutoloader
+ * @TODO redesign to not use the DI
  */
 class InstanceConfigAutoloader
 {
     const DATA_DIR_NAME = '/data';
     const DATA_SUFFIX   = '.data.php';
 
+    /**
+     * @var string
+     */
     protected $dir;
+
+    /**
+     * @var array
+     */
     protected $merge = [];
 
+    /**
+     * @param $dir
+     */
     public function __construct($dir)
     {
         $this->dir = $dir;
     }
 
+    /**
+     * @param array $subject
+     * @return $this
+     */
     public function merge(array $subject)
     {
         $this->merge = ArrayUtils::merge($this->merge, $subject);
         return $this;
     }
 
+    /**
+     * @return array
+     */
     public function toArray()
     {
         $dir = $this->dir . self::DATA_DIR_NAME;
@@ -67,19 +88,23 @@ class InstanceConfigAutoloader
             $itemName   = str_replace('-', '_', explode('.', $fInfo['filename'], 2)[0]);
             $tableIndex = substr($index, 0, strlen($index) - 4) . 'Table';
 
-            $config['alias'][$tableIndex] = 'Zend\Db\TableGateway\TableGateway';
-            $config['alias'][$index]      = 'WebinoData\DataService';
+            $config['alias'][$tableIndex] = TableGateway::class;
+            $config['alias'][$index]      = DataService::class;
 
             $config[$tableIndex] = [
                 'parameters' => [
                     'table'   => $itemName,
-                    'adapter' => 'Zend\Db\Adapter\Adapter',
+                    'adapter' => Adapter::class,
                 ],
             ];
 
             /** @noinspection PhpIncludeInspection */
             $dataConfig = require $path[0];
             $config[$index] = is_array($dataConfig) ? $dataConfig : $dataConfig->toArray();
+
+            // set default table
+            empty($config[$index]['parameters']['tableGateway'])
+                and $config[$index]['parameters']['tableGateway'] = $tableIndex;
         }
 
         return array_replace_recursive($config, $this->merge);
@@ -88,6 +113,7 @@ class InstanceConfigAutoloader
     /**
      * @param string $dir
      * @return RegexIterator
+     * @TODO common
      */
     private function createDirIterator($dir)
     {
