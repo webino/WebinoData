8443a8eee0acd66d6b8bdcbf9da04aedff58b4bf
Peter Bacinsky <peter@bacinsky.sk>
2016-05-26 21:13:59 +0200

    Updated Config\InputFilter\Input\DefaultableTrait & RequirableTrait, fixed return hint - use  instead of self

--- src/WebinoData/Config/InputFilter/Input/DefaultableTrait.php
+++ src/WebinoData/Config/InputFilter/Input/DefaultableTrait.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoData for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2016 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -17,7 +17,7 @@ trait DefaultableTrait
 {
     /**
      * @param int|string|bool|null $value
-     * @return self
+     * @return $this
      */
     public function setDefault($value)
     {
