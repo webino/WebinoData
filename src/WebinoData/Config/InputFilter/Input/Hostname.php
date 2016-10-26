35ca90936833a3a958806b05679a8d1ea6a9aac9
Peter Bacinsky <peter@bacinsky.sk>
2016-10-26 09:37:22 +0200

    Added Config\InputFilter\Input\Hostname

---
+++ src/WebinoData/Config/InputFilter/Input/Hostname.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoData for the canonical source repository
+ * @copyright   Copyright (c) 2012-2016 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     BSD-3-Clause
+ */
+
+namespace WebinoData\Config\InputFilter\Input;
+
+/**
+ * Hostname data type
+ */
+class Hostname extends AbstractInput
+{
+    use RequirableTrait;
+    use DefaultableTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $spec = [
+        'validators' => [
+            'hostname' => ['name' => 'Hostname'],
+        ],
+    ];
+
+    /**
+     * @param string $name
+     */
+    public function __construct($name)
+    {
+        $this->setName($name);
+    }
+}
