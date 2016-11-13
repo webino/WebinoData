2cdf938275df78431aa173228de5f88472c2cb4a
Peter Bacinsky <peter@bacinsky.sk>
2016-11-13 11:41:54 +0100

    Updated Digit input configurator, added setBetween()

--- src/WebinoData/Config/InputFilter/Input/Digit.php
+++ src/WebinoData/Config/InputFilter/Input/Digit.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoData for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2016 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -33,5 +33,22 @@ class Digit extends AbstractInput
     public function __construct($name)
     {
         $this->setName($name);
+    }
+
+    /**
+     * @param int $min
+     * @param int $max
+     * @return $this
+     */
+    public function setBetween($min, $max)
+    {
+        $this->spec['validators'] = [
+            'between' => [
+                'name'    => 'Between',
+                'options' => ['min' => $min, 'max' => $max],
+            ],
+        ];
+
+        return $this;
     }
 }
