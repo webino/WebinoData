25237d10d61524f507bc0c6c6dd023a0b2bc6f4f
Peter Bacinsky <peter@bacinsky.sk>
2018-05-20 13:30:29 +0200

    Updated

--- tests/resources/data/db/schema.sql
+++ tests/resources/data/db/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE main (
   sub_id  int(10) NOT NULL,
   sub2_id int(10) NOT NULL,
   PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-ALTER TABLE main_sub ADD INDEX `main has many sub` (mainid), ADD CONSTRAINT `main has many sub` FOREIGN KEY (mainid) REFERENCES main (id) ON UPDATE Cascade ON DELETE Cascade;
-ALTER TABLE main_sub ADD INDEX `sub has many main` (subid), ADD CONSTRAINT `sub has many main` FOREIGN KEY (subid) REFERENCES sub (id) ON UPDATE Cascade ON DELETE Cascade;
-ALTER TABLE main ADD INDEX `main has sub2` (sub2_id), ADD CONSTRAINT `main has sub2` FOREIGN KEY (sub2_id) REFERENCES sub (id) ON UPDATE Cascade ON DELETE Cascade;
-ALTER TABLE main ADD INDEX `main has sub` (sub_id), ADD CONSTRAINT `main has sub` FOREIGN KEY (sub_id) REFERENCES sub (id) ON UPDATE Cascade ON DELETE Cascade;
+ALTER TABLE main_sub ADD INDEX `main_has_many_sub` (mainid), ADD CONSTRAINT `main_has_many_sub` FOREIGN KEY (mainid) REFERENCES main (id) ON UPDATE Cascade ON DELETE Cascade;
+ALTER TABLE main_sub ADD INDEX `sub_has_many_main` (subid), ADD CONSTRAINT `sub_has_many_main` FOREIGN KEY (subid) REFERENCES sub (id) ON UPDATE Cascade ON DELETE Cascade;
+ALTER TABLE main ADD INDEX `main_has_sub2` (sub2_id), ADD CONSTRAINT `main_has_sub2` FOREIGN KEY (sub2_id) REFERENCES sub (id) ON UPDATE Cascade ON DELETE Cascade;
+ALTER TABLE main ADD INDEX `main_has_sub` (sub_id), ADD CONSTRAINT `main_has_sub` FOREIGN KEY (sub_id) REFERENCES sub (id) ON UPDATE Cascade ON DELETE Cascade;
